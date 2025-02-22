154b12d4638c14be70453aabec11292f4d0873dd
Kevin El-Khoury <kevin.elkhoury0@gmail.com>
2025-02-22 09:21:15 -0500

    add map markers

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import './style.css'
 
+// Initialize leaflet map
 var map = L.map('map').setView([43.6532, -79.3832], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+function onMapClick(e) {
+  L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+}
+
+map.on('click', onMapClick);
