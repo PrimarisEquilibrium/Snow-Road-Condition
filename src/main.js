2e36edb55ff17d9bc4a1d10f7a9649934ebf8a31
Kevin El-Khoury <kevin.elkhoury0@gmail.com>
2025-02-22 09:12:34 -0500

    add leafletjs map

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import './style.css'
+
+var map = L.map('map').setView([43.6532, -79.3832], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
